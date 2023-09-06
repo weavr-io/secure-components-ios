2d28b461d40eb901cd1e402f6bf0e87ef621676e
stephen-galea-weavr <stephen.galea@weavr.io>
2023-09-06 14:36:56 +0000

    Release 1.4.3

---
+++ WeavrComponents.xcframework/ios-arm64_x86_64-simulator/WeavrComponents.framework/Headers/components.h
@@ -0,0 +1,22 @@
+//
+//  components.h
+//  components
+//
+//  Created by Tareq on 12/4/22.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for components.
+FOUNDATION_EXPORT double componentsVersionNumber;
+
+//! Project version string for components.
+FOUNDATION_EXPORT const unsigned char componentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <components/PublicHeader.h>
+
+//#import <PSA/PSA.h>
+//#import <PSA/ResourceProvider.h>
+//#import <PSA/PSASharedStatuses.h>
+//#import <PSA/PSATenant.h>
