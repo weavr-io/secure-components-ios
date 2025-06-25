c978f5697d4c13671f34f27892f930e4a9c7af2e
stephen-galea-weavr <stephen.galea@weavr.io>
2025-06-25 14:07:03 +0000

    Added Release 3.0.0

--- WeavrComponents.xcframework/ios-arm64/WeavrComponents.framework/Headers/WeavrComponents-Swift.h
+++ WeavrComponents.xcframework/ios-arm64/WeavrComponents.framework/Headers/WeavrComponents-Swift.h
@@ -305,6 +305,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
+
 @class NSCoder;
 
 SWIFT_CLASS("_TtC15WeavrComponents11SecureLabel")
