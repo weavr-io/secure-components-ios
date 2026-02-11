356d2b0aa0515b368339b72db9a8647ee292521d
stephen-galea-weavr <stephen.galea@weavr.io>
2026-02-11 15:09:16 +0000

    Added Release 3.1.5

--- WeavrComponents.xcframework/ios-arm64/WeavrComponents.framework/Headers/WeavrComponents-Swift.h
+++ WeavrComponents.xcframework/ios-arm64/WeavrComponents.framework/Headers/WeavrComponents-Swift.h
@@ -329,7 +329,6 @@ SWIFT_CLASS("_TtC15WeavrComponents21SecureCardNumberLabel")
 @end
 
 @class NSAttributedString;
-@class UITextPosition;
 @class UITextRange;
 SWIFT_CLASS("_TtC15WeavrComponents15SecureTextField")
 @interface SecureTextField : UITextField
@@ -338,7 +337,6 @@ SWIFT_CLASS("_TtC15WeavrComponents15SecureTextField")
 @property (nonatomic, copy) NSString * _Nullable text;
 @property (nonatomic, strong) NSAttributedString * _Nullable attributedText;
 @property (nonatomic, getter=isSecureTextEntry) BOOL secureTextEntry;
-- (UITextRange * _Nullable)textRangeFromPosition:(UITextPosition * _Nonnull)_ toPosition:(UITextPosition * _Nonnull)_ SWIFT_WARN_UNUSED_RESULT;
 - (NSString * _Nullable)textInRange:(UITextRange * _Nonnull)_ SWIFT_WARN_UNUSED_RESULT;
 @property (nonatomic) UIKeyboardType keyboardType;
 @end
